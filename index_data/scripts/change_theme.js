4c51bc9cc5e1e8e5dad61db21d58c9dc69089fe8
DragonHtmIL <meirfons1@gmail.com>
2025-05-15 00:57:08 +0300

    Delete index_data directory

--- index_data/scripts/change_theme.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("themeCheckbox").addEventListener('change', switchTheme, false);
-if (document.documentElement.getAttribute("data-theme") == "dark"){
-  document.getElementById("themeCheckbox").checked = true;
-  document.querySelector('meta[name="theme-color"]').setAttribute('content', '#0c0c0c');
-};
--- index_data/themes/system/style_loader.js
+++
@@ -1,8 +0,0 @@
-function styleLoadCss() {
-  const rootlit = document.getElementById("styleRootLit");
-  const rootdark = document.getElementById("styleRootDark");
-  const style = document.getElementById("styleBase");
-  rootlit.setAttribute("href", localStorage.getItem("themeLocator") + "/root_light.css");
-  rootdark.setAttribute("href", localStorage.getItem("themeLocator") + "/root_dark.css");
-  style.setAttribute("href", localStorage.getItem("themeLocator") + "/style_0.css");
-}
