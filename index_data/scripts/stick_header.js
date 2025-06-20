6e2c90017df1cd44dd8805be28a2736a9347ff68
DragonHtmIL <meirfons1@gmail.com>
2025-06-20 11:33:30 +0300

    Delete index_data directory

--- index_data/scripts/stick_header.js
+++
@@ -1,9 +0,0 @@
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-};
